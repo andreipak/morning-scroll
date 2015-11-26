bbe01a348b86df06eb14f492c1fcc5d3a6f1ea10
Josh Cho <joshchonpc@gmail.com>
2015-11-26 11:25:54 +0900

    modest framework

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 import sys
 import feedparser
 
+feedlist = ["https://www.google.com/alerts/feeds/03016667448555882041/10813419956796523147",
+            "https://www.google.com/alerts/feeds/03016667448555882041/18371980089906002277",
+            "https://www.google.com/alerts/feeds/03016667448555882041/1720301016679370235"]
+
+# def read_feedlist(filename)
+
+# def valid(entry)
+
 def main(): 
-  print ('Hello there', sys.argv[1])
+    # feedlist = read_feedlist("feedlist.txt")
+    for url in feedlist:
+        d = feedparser.parse(url)
+        print(d.feed.title)
+        entries = filter(valid, d.entries)
+        for entry in entries:
+            print(entry.title)
 
 # Boilerplate
 if __name__ == '__main__':
-  main()
+    main()
