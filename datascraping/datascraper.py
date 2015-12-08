bb3621306073569e2b08aba34efeb1314238479e
Josh Cho <joshchonpc@gmail.com>
2015-12-08 16:52:53 +0900

    failed restructuring, tries to make nested happen

--- datascraping/datascraper.py
+++ datascraping/datascraper.py
@@ -1,65 +1,72 @@
 import sys
-import libs.feedparser.feedparser
-import json # for reading json files
+import webapp2
+import lib.feedparser
+import lib.simplejson.encoder
+import lib.simplejson.decoder
 from collections import defaultdict # for initialization
-from libs.termcolor import colored # for savvy debugging
-import libs.schedule # for scheduled stuff
+from termcolor import colored # for savvy debugging
+import lib.schedule # for scheduled stuff
 import time
 from newschunk import NewsChunk
 import difflib # for getting rid of duplicate-like articles
-import datetime
-import libs.PyRSS2Gen # for generating feed
+from datetime import datetime
+import lib.PyRSS2Gen # for generating feed
+from google.appengine.ext import db
 
-feednames_src = "metalists/kr_feednames"
-hitlistnames_src = "metalists/kr_hitlistnames"
+feednames_src = "metalists/en_feednames"
+hitlistnames_src = "metalists/en_hitlistnames"
 PATH_TO_DATA = "hitlists/"
 DATA_EXTENSION = ".json"
 # 0.44 seems to catch most things, could be improved by weighting our hit-terms
 # more
 DUPLICATE_THRESHOLD = 0.44
 
-PRINT_ALL = False
-def print_info(newschunks):
-    if len(newschunks) <= 0:
-        return
-    notImportant = []
-    almostImportant = []
-    important = []
-    for title in newschunks:
-        nc = newschunks[title]
-        if nc.worthShowing():
-            important.append(nc)
-        elif nc.getWeight() > 0:
-            almostImportant.append(nc)
-        else:
-            notImportant.append(nc)
+# PRINT_ALL = False
+# def print_info(newschunks):
+#     if len(newschunks) <= 0:
+#         return
+#     notImportant = []
+#     almostImportant = []
+#     important = []
+#     for title in newschunks:
+#         nc = newschunks[title]
+#         if nc.worthShowing():
+#             important.append(nc)
+#         elif nc.weight > 0:
+#             almostImportant.append(nc)
+#         else:
+#             notImportant.append(nc)
+#
+#     # sorts by weight
+#     almostImportant.sort(key=lambda x: x.weight, reverse=False)
+#     important.sort(key=lambda x: x.weight, reverse=False)
+#
+#     if PRINT_ALL:
+#         # activate for algorithm optimization, floods the stuff a lot
+#         if len(notImportant) > 0:
+#             for nimp in notImportant:
+#                 nimp.print_info()
+#             print
+#             print("\t---------------------------------------")
+#             print
+#
+#     if len(almostImportant) > 0:
+#         for almostimp in almostImportant:
+#             almostimp.print_info()
+#         print
+#         print("\t---------------------------------------")
+#         print
+#
+#     if len(important) > 0:
+#         for imp in important:
+#             imp.print_info()
+#         print
+#         print("\t---------------------------------------")
+#         print
 
-    # sorts by weight
-    almostImportant.sort(key=lambda x: x.getWeight(), reverse=False)
-    important.sort(key=lambda x: x.getWeight(), reverse=False)
-
-    if PRINT_ALL:
-        # activate for algorithm optimization, floods the stuff a lot
-        if len(notImportant) > 0:
-            for nimp in notImportant:
-                nimp.print_info()
-            print
-            print("\t---------------------------------------")
-            print
-
-    if len(almostImportant) > 0:
-        for almostimp in almostImportant:
-            almostimp.print_info()
-        print
-        print("\t---------------------------------------")
-        print
-
-    if len(important) > 0:
-        for imp in important:
-            imp.print_info()
-        print
-        print("\t---------------------------------------")
-        print
+class DictModel(db.Model):
+    def to_dict(self):
+        return dict([(p, unicode(getattr(self, p))) for p in self.properties()])
 
 class DataScraper(object):
 
@@ -82,7 +89,7 @@ class DataScraper(object):
         for hitlistname in self.hitlistnames:
             print(hitlistname)
             with open(PATH_TO_DATA + hitlistname + DATA_EXTENSION) as data_file:
-                tmp = json.load(data_file)
+                tmp = lib.simplejson.decoder.load(data_file)
                 self.data_of[hitlistname] = tmp[hitlistname]
                 # data_of is now a proper dictionary of list of dictionaries!
                 # Valid use would be 'data_of[filtername][index]["title"]'
@@ -90,15 +97,15 @@ class DataScraper(object):
         print
 
     def add_nc(self, new_nc):
-        self.newschunks[new_nc.getTitle()] = new_nc
-        self.archive_newschunks[new_nc.getTitle()] = new_nc
+        self.newschunks[new_nc.title] = new_nc
+        self.archive_newschunks[new_nc.title] = new_nc
 
     # deletes a newschunk from the dictionary (also deletes the key)
     def del_nc(self, old_nc):
         # del the original
         if old_nc is not None:
-            self.newschunks.pop(old_nc.getTitle())
-            self.archive_newschunks.pop(old_nc.getTitle())
+            self.newschunks.pop(old_nc.title)
+            self.archive_newschunks.pop(old_nc.title)
         else:
             print("ERROR: Could not delete properly")
 
@@ -121,15 +128,29 @@ class DataScraper(object):
                     match_nc = self.archive_newschunks[existing_title]
                     break
 
-            new_nc = NewsChunk(entry)
+            # if entry.created is not None:
+            #     entry.created   = str(entry.created)
+            # if entry.updated is not None:
+            #     entry.updated   = str(entry.updated)
+            # if entry.updated_parsed is not None:
+            #     entry.updated_parsed   = str(entry.updated_parsed)
+            # # if entry.expired is not None:
+            # #     entry.expired   = str(entry.expired)
+            # if entry.published is not None:
+            #     entry.published = str(entry.published)
+            # if entry.published_parsed is not None:
+            #     entry.published_parsed = str(entry.published_parsed)
+
+            new_nc = NewsChunk(entry_json = simplejson.encoder.dumps(entry.to_dict()))
             for fn in self.data_of:
                 for hit in self.data_of[fn]:
                     if hit["title"] in entry.title.lower(): # it's a hit!
-                        new_nc.addHit(hit)
+                        new_nc.hitnames.append(hit["title"])
+                        new_nc.weight += hit["weight"]
 
             if match_nc is None:
                 self.add_nc(new_nc)
-            elif new_nc.getWeight() > match_nc.getWeight():
+            elif new_nc.weight > match_nc.weight:
                 # similar, but more important
                 self.del_nc(match_nc)
                 self.add_nc(new_nc)
@@ -156,7 +177,7 @@ class DataScraper(object):
             nc = self.archive_newschunks[title]
             # if not nc.worthShowing():
             #     continue
-            x = nc.getEntry()
+            x = simplejson.decoder.loads(nc.entry)
             items.append(libs.PyRSS2Gen.RSSItem(
                 title = x.title,
                 link = x.link,
@@ -172,8 +193,8 @@ class DataScraper(object):
 
                 language = "en",
                 copyright = "",
-                pubDate = datetime.datetime.now(),
-                lastBuildDate = datetime.datetime.now(),
+                pubDate = datetime.now(),
+                lastBuildDate = datetime.now(),
 
                 categories = "",
                 generator = "",
